42151d46026064d2c216cac38442a60488629c85
ArtemSpiz <artemijspizhav@gmail.com>
2025-08-06 13:04:35 +0300

    f

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HeaderLayout from "./layout/Header/HeaderLayout.vue";
 import Hero from "./components/Hero/Hero.vue";
 import Accessibility from "./components/Accessibility/Accessibility.vue";
 import Scale from "./components/Scale/Scale.vue";
-// import Future from "./components/Future/Future.vue";
+import Future from "./components/Future/Future.vue";
 import Roadmap from "./components/Roadmap/Roadmap.vue";
 import FIX from "./components/FIX/FIX.vue";
 import FAQ from "./components/FAQ/FAQ.vue";
@@ -54,11 +54,11 @@ onMounted(() => {
     :isOnRoadmap="isHeaderDark"
   />
   <Hero />
-  <Accessibility />
+  <!-- <Accessibility /> -->
   <FIX />
   <Scale />
-  <!-- <Future /> -->
-  <Roadmap />
+  <Future />
+  <!-- <Roadmap /> -->
   <FAQ />
   <Institutions />
 </template>

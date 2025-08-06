2d5f0c0e04dc8b75e4a2a8e5e31bbf9a1af51b4b
ArtemSpiz <artemijspizhav@gmail.com>
2025-08-06 13:05:33 +0300

    f

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 import "./style.css";
 import HeaderLayout from "./layout/Header/HeaderLayout.vue";
 import Hero from "./components/Hero/Hero.vue";
-import Accessibility from "./components/Accessibility/Accessibility.vue";
+// import Accessibility from "./components/Accessibility/Accessibility.vue";
 import Scale from "./components/Scale/Scale.vue";
 import Future from "./components/Future/Future.vue";
-import Roadmap from "./components/Roadmap/Roadmap.vue";
+// import Roadmap from "./components/Roadmap/Roadmap.vue";
 import FIX from "./components/FIX/FIX.vue";
 import FAQ from "./components/FAQ/FAQ.vue";
 import Institutions from "./components/Institutions/Institutions.vue";
